370e289bd28700ae5d25bad930ba4b2e8044a643
dennis <ieatnubcake@gmail.com>
2025-01-05 11:39:05 -0800

    render server up

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
             showTypingIndicator();
             
             try {
-                const response = await fetch('http://localhost:3000/chat', {
+                const response = await fetch('https://ollama-server-ceiw.onrender.com/chat', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
